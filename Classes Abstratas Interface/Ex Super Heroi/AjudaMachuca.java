eba0fd9d856709fc23e3e08a310360f9ba6cba19
201819060144 <201819060144@lab02-23.dominio.facom.ufms.br>
2018-09-11 18:49:44 -0400

    lucas

---
+++ Classes Abstratas Interface/Ex Super Heroi/AjudaMachuca.java
@@ -0,0 +1,11 @@
+
+public abstract class AjudaMachuca {
+
+	/***********/
+	/* MÉTODOS */
+	/***********/
+	
+	public int ajuda(SuperHeroi ajudado);
+	
+	public int machuca(SuperHeroi machucado);
+}
---
+++ Classes Abstratas Interface/Ex Super Heroi/SuperHeroi.java
@@ -0,0 +1,13 @@
+
+public abstract class SuperHeroi implements AjudaMachuca{
+
+	private String nomeOriginal;
+	private String descricao;
+	private Data dataDeCriacao;
+	private String paisNatal;
+	private int nivelDeResistencia;
+	private EstadoDeSaude estadoDeSaude;
+	
+	
+	
+}
